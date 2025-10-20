de29a408852e4fd8cdb764513803a83abda035d0
sylvi <20102981@mail.wit.ie>
2025-10-20 23:47:57 +0100

    Exercise: Movie Details Page- included production countries.

--- src/components/movieDetails/index.jsx
+++ src/components/movieDetails/index.jsx
@@ -59,12 +59,25 @@ const MovieDetails = ({ movie }) => {  // Don't miss this!
           icon={<LanguageIcon />}
           label={`Original Language: ${movie.original_language}`}
         />
-        <Chip  />
          <Chip
           icon={<StarRate />}
           label={`${movie.vote_average} (${movie.vote_count})`}
         />
         <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
+
+       <Paper
+        component="ul"
+        sx={{ ...root }}
+      >
+        <li>
+          <Chip label="Production Countries" sx={{ ...chip }} color="primary" />
+        </li>
+        {movie.production_countries.map((p) => (
+          <li key={p.name}>
+            <Chip label={p.name} sx={{ ...chip }} />
+          </li>
+        ))}
       </Paper>
 
       <Fab
